5d5ebc58ab88184cb06d449c7943cfac0ca1854c
Sergey <smirsand@mail.ru>
2023-07-20 09:25:56 +0300

    code change

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE customers (
 
 
 CREATE TABLE employees (
-    employee_id INT NOT NULL,
+    employee_id INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     title VARCHAR(100) NOT NULL,
@@ -17,9 +17,9 @@ CREATE TABLE employees (
 
 
 CREATE TABLE orders (
-    order_id INT NOT NULL,
-	customer_id INT REFERENCES customers(customer_id) NOT NULL,
-	employee_id INT REFERENCES employees(employee_id) NOT NULL,
+    order_id INT PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id),
+	employee_id INT REFERENCES employees(employee_id),
 	order_date DATE NOT NULL,
 	ship_city VARCHAR(50) NOT NULL
 );
